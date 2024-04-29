41afd3b94026c27c0d2b87fc0873440049d635d9
léandéré <leandre.mascher.pro@gmail.com>
2024-04-29 19:45:09 +0200

    :package: bulma added

--- larablog/resources/views/layouts/app.blade.php
+++ larablog/resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
